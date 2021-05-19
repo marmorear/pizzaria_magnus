77973e92e61c55e1517a94a8d59986b3fa12e3d0
marmorear <52580594+marmorear@users.noreply.github.com>
2021-05-19 19:12:22 -0300

    Update conn.php

--- conn.php
+++ conn.php
@@ -2,7 +2,6 @@
 <?php
 try {
 $conn = new PDO('mysql:host=localhost;dbname=pizza149_magnus', "root", "");
-    //$conn = new PDO('mysql:host=localhost;dbname=pizza149_magnus', "pizza149_site", "xolHq4!Sa4GI");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $conn->exec("set names utf8");
 } catch (PDOException $erro) {
